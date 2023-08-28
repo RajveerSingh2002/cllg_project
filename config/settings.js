df90c2bb8fa8ea4ded05b59ad59f71faffea2391
RajveerSingh2002 <109398658+RajveerSingh2002@users.noreply.github.com>
2023-08-28 13:22:57 +0530

    Add files via upload

---
+++ config/settings.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+//   mongoDBUrl:
+//     "mongodb://127.0.0.1:27020/gtacars",
+	mongoDBUrl: "mongodb+srv://balrajhumber:h6ojEE9e4b6eOx3u@cluster0.6xykjaq.mongodb.net/gtacars?retryWrites=true&w=majority",
+  secret: "verySecretCode",
+};
